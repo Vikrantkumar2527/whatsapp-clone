d9b163cc49b1e7d4d089b23a0fa0e14ce04f2262
TanuSingh2821 <ts626700@gmail.com>
2025-05-10 00:55:25 +0530

    Updated API host to use deployed backend

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST ="http://localhost:3005";
+export const HOST ="https://whatsapp-clone-10.onrender.com";
 const AUTH_ROUTE =`${HOST}/api/auth`;
 const MESSAGES_ROUTE=`${HOST}/api/messages`;
 export const CHECK_USER_ROUTE =`${AUTH_ROUTE}/check-user`;
